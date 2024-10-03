f720983138c2e5142bb3d37de0741fb969594bd3
“[firstname <himakarhimmu@gmail.com>
2024-10-03 16:39:20 +0530

    CSV and JSON Functions additon

--- src/utilts.js
+++ src/utilts.js
@@ -1,4 +1,6 @@
 const fs = require("fs");
+const parse = require("csv-parse/lib/sync"); // You can use the csv-parse package
+const { stringify } = require("csv-stringify/sync");
 
 function readFile(filePath) {
   return fs.readFileSync(filePath, "utf-8");
@@ -26,9 +28,37 @@ function validateFile(filePath) {
   return { valid: true, message: "File is valid" };
 }
 
+function readCSV(filePath) {
+  const fileContent = fs.readFileSync(filePath);
+  const records = parse(fileContent, {
+    columns: true, // Use first row as header
+    skip_empty_lines: true,
+  });
+  return records; // Returns an array of objects
+}
+
+function writeCSV(filePath, data) {
+  const csvContent = stringify(data, { header: true });
+  fs.writeFileSync(filePath, csvContent);
+}
+
+function readJSON(filePath) {
+  const data = fs.readFileSync(filePath);
+  return JSON.parse(data); // Returns the parsed object
+}
+
+function writeJSON(filePath, data) {
+  const jsonData = JSON.stringify(data, null, 2); // Pretty print with 2 spaces
+  fs.writeFileSync(filePath, jsonData);
+}
+
 module.exports = {
   readFile,
   writeFile,
   appendToFile,
   validateFile,
+  readCSV,
+  writeCSV,
+  readJSON,
+  writeJSON,
 };
